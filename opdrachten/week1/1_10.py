5d3315292b9caf074163122cd5065202321af561
Youri de Vor <43245732+youridv1@users.noreply.github.com>
2019-11-12 19:55:28 +0100

    Create 1_10.py

---
+++ opdrachten/week1/1_10.py
@@ -0,0 +1,28 @@
+def moneyPossibilities(grens: int, amount: int, geld: list, matrix: list):
+    if matrix[grens][amount] == -1:
+        if amount == 0:
+            matrix[grens][amount] = 1
+        if grens == 0:
+            matrix[grens][amount] = 1
+        if amount >= geld[grens]:
+            matrix[grens][amount] = moneyPossibilities(grens-1, amount, geld, matrix) + moneyPossibilities(grens, amount-geld[grens], geld, matrix)
+        if amount < geld[grens]:
+            matrix[grens][amount] = moneyPossibilities(grens-1, amount, geld, matrix)
+    return matrix[grens][amount]
+
+
+
+def main(amount: int):
+    geld = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
+    matrix = []
+    lijst = []
+
+    for i in range(amount+1):
+        lijst.append(-1)
+    for i in range(len(geld)):
+        matrix.append(lijst)
+
+    print(moneyPossibilities(12, amount, geld, matrix))
+
+main(1)
+
